9be812ee4111b60ecc849914ab438a02f675e6c1
iamshivamgaur <shivam24august@gmail.com>
2025-08-27 03:53:46 +0530

    checking userAuth and storing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,53 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "./components/Header";
+import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./utils/firebase.js";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "./store/slices/userSlice";
+
+const toastOptions = {
+  duration: 4000,
+  style: {
+    background: "#333",
+    color: "#fff",
+  },
+  success: {
+    duration: 3000,
+    style: {
+      background: "#016630 ",
+    },
+  },
+  error: {
+    duration: 5000,
+    style: {
+      background: "#9f0712",
+    },
+  },
+};
 
 function App() {
-  return (
-    <div
-      className="w-full min-h-screen bg-cover bg-center bg-black"
-      style={{
-        backgroundImage: "url('/netflix-bg.jpg')",
-      }}
-    >
-      <div className="absolute inset-0 bg-black/70"></div>
+  const dispatch = useDispatch();
 
-      <div className="z-10 relative">
-        <Header />
-        <Outlet />
-      </div>
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { displayName, uid, email } = user;
+        dispatch(addUser({ uid, email, displayName }));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
+  return (
+    <div className="w-full min-h-screen relative bg-black">
+      <Header />
+      <Outlet /> {/* page content goes here */}
+      <Toaster position="top-right" toastOptions={toastOptions} />
     </div>
   );
 }
